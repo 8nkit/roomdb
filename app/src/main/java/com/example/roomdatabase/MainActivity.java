80dcc6371e6d7954339df82e1565a0a22a0cf8c5
Ankit Sharma <ankitvashisht@hotmail.com>
2021-02-04 13:06:28 +0530

    TASK 1 Finished--- 2 record in code insertion to RoomDB

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         StudentRepository repository=new StudentRepository(getApplicationContext());
+        Student student=new Student("101","James Trudo","238293819","M");
+        Student student2=new Student("102","Frank K. Trudo","238293819","M");
+        repository.InsertTask(student);
+        repository.InsertTask(student2);
     }
 }
--- app/src/main/java/com/example/roomdatabase/StudentRepository.java
+++ app/src/main/java/com/example/roomdatabase/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.example.roomdatabase;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.widget.Toast;
 
 import androidx.room.Room;
@@ -22,4 +23,23 @@ public class StudentRepository {
 
         Toast.makeText(context,"Database Creation Sucessful",Toast.LENGTH_SHORT).show();
     }
+
+    public  void InsertTask(final Student student){
+        new AsyncTask<Void,Void,Void>(){
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+            studentDatabase.studentDAO().insertTask(student);
+            return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(context,student.getName()+" Insertion Sucessful",Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }
+
+
 }
